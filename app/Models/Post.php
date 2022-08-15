f09349b23d95601a9a905f88d4f436933a4389aa
NaingLynnAung <unrivaledking1@gmail.com>
2022-08-15 09:24:45 +0630

    learn accessor and mutator

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,12 +3,41 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'description'];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d',
+        'updated_at' => 'datetime:Y-m-d',
+    ];
+    protected $appends = [
+        'path'
+    ];
+
+    public function path() : Attribute 
+    {
+        return new Attribute(fn () => route('posts.show', $this));
+    }
+
+    public function title() : Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => ucwords($value),
+            set: fn ($value) => strtolower($value)
+        );
+    }
+
+    public function description() : Attribute 
+    {
+        return new Attribute(
+            fn ($value) => ucfirst($value),
+        );
+    }
 
     public function user()
     {
